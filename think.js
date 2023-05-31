924e8073704984abba142ef9d6977788c59e16f4
rebecapoma6 <mrebecapoma.6@gmail.com>
2023-05-31 23:55:17 +0200

    changesRebeca

--- think.js
+++ think.js
@@ -1,4 +1,4 @@
-console.log ("Hola Mundo");
-console.log ("Soy Anthony Brayan");
-console.log ("Soy Rebeca");
-console.log ("O");
+const dateControl = document.querySelector('input[type="date"]');
+dateControl.value = '2023-06-01';
+console.log(dateControl.value); // prints "2017-06-01"
+console.log(dateControl.valueAsNumber); // prints 1496275200000, a JavaScript timestamp (ms)
