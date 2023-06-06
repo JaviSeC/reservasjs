c3bd2fd3a0d36cfaa1ee9045e03163cb712595c9
rebecapoma6 <mrebecapoma.6@gmail.com>
2023-06-06 12:29:54 +0200

    cambio ahora bien

--- scripts/think.js
+++ scripts/think.js
@@ -1,11 +1,24 @@
-console.log ("Hola Mundo");
-console.log ("Soy Anthony Brayan");
-console.log ("Soy Rebeca");
-<<<<<<< HEAD:think.js
-console.log ("Soy Javier");
-console.log ("Soy Javier, Anthony, Rebeca");
-console.log("rebeca poma");
-console.log("tengo sueño");
-=======
-console.log ("O");
->>>>>>> dev:scripts/think.js
+
+function addRow(){    
+
+let plays = document.getElementById('play-tb').value;
+let fecha = document.getElementById('start-date').value;
+let old = document.getElementById('old-people-age').value;
+let boys = document.getElementById('children').value;
+
+let table = document.getElementById('table-list-play');
+
+let newRow = table.insertRow();
+let playsCell = newRow.incertCell();
+let fechaCell = newRow.incertCell();
+let oldCell = newRow.incerCell();
+let boysCell = newRow.incertCell();
+
+playsCell.innerHTML = plays;
+fechaCell.innerHTML = fecha;
+oldCell.innerHTML = old;
+boysCell.innerHTML = boys;
+
+//alert("onclickfuncionando")
+
+}
