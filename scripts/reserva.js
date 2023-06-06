95ee6b074c73e34bd0e2b8de4b501ce88f9aff87
Anthony Brayan <brayan.farronan@gmail.com>
2023-06-06 13:59:22 +0200

    Parte de rebe ok

--- scripts/reserva.js
+++ scripts/reserva.js
@@ -1,17 +1,24 @@
-//function agregarDatosTb() {    
 
-    //let plays = document.getElementById('play-tb').value;
-    
-    //let table = document.getElementById('table-list-play');
-    
-    //let juegonew = table.insertRow();
-    //let nameCell = juegonew.insertCell();
-    //let nameCell = juegonew.insertCell();
-    //let nameCell = juegonew.insertCell();
-    
-    
-    //nameCell.innerHTML = plays;
-    
-    
-    //}
-    
+function addRow(){    
+
+let plays = document.getElementById('play-tb').value;
+let fecha = document.getElementById('start-date').value;
+let old = document.getElementById('old-people-age').value;
+let boys = document.getElementById('children').value;
+
+let table = document.getElementById('table-list-play');
+
+let newRow = table.insertRow();
+let playsCell = newRow.insertCell();
+let fechaCell = newRow.insertCell();
+let oldCell = newRow.insertCell();
+let boysCell = newRow.insertCell();
+
+playsCell.innerHTML = plays;
+fechaCell.innerHTML = fecha;
+oldCell.innerHTML = old;
+boysCell.innerHTML = boys;
+
+//alert("onclickfuncionando")
+
+}
