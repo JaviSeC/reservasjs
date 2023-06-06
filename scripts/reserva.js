0903beb263646a8c9c7be834a8ebfd448ba37e1f
Anthony Brayan <brayan.farronan@gmail.com>
2023-06-06 13:47:44 +0200

    restableciendo cambios

---
+++ scripts/reserva.js
@@ -0,0 +1,17 @@
+//function agregarDatosTb() {    
+
+    //let plays = document.getElementById('play-tb').value;
+    
+    //let table = document.getElementById('table-list-play');
+    
+    //let juegonew = table.insertRow();
+    //let nameCell = juegonew.insertCell();
+    //let nameCell = juegonew.insertCell();
+    //let nameCell = juegonew.insertCell();
+    
+    
+    //nameCell.innerHTML = plays;
+    
+    
+    //}
+    
